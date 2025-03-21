0fd6b5df916fe5e51300d09f64ef5ee8794f748c
Kamil Erguler <kerguler@gmail.com>
2025-03-21 16:49:27 +0200

    take 2

--- workshop-website/gatsby-config.js
+++ workshop-website/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Climate-Sensitive Vector Dynamics Modelling`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    author: `@kerguler`,
+    siteUrl: `https://www.vectormodelling.com/`,
   },
   plugins: [
     `gatsby-plugin-image`,
