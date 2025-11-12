e4508cea3400ccaafaf8af5220ca33eda0d963e3
danddr <dare.daniele@gmail.com>
2025-11-12 13:38:53 +0100

    Updates

---
+++ component---src-pages-trento-2026-404-js-4dd35fd3671819316d6a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[941],{2698:function(t,e,n){n.r(e),n.d(e,{Head:function(){return r}});var a=n(6540),s=n(1221),u=n(7528);const r=()=>a.createElement(u.A,{title:"404: Not Found"});e.default=()=>a.createElement(s.A,{workshop:"Trento2026"},a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-trento-2026-404-js-4dd35fd3671819316d6a.js.map
