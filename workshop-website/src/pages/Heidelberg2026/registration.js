f69b40b6b5995a269ad34e0097cb0b9f5c21d95d
danddr <dare.daniele@gmail.com>
2026-07-18 16:40:32 +0200

    update registration date

--- workshop-website/src/pages/Heidelberg2026/registration.js
+++ workshop-website/src/pages/Heidelberg2026/registration.js
@@ -12,7 +12,7 @@ const RegistrationPage = () => (
     There are <b>no registration fees</b> for attending the workshop. <b>However, places are limited</b> due to logistical constraints and to foster active participation and meaningful discussion. Given these constraints, invitations to attend will be extended by our Scientific Committee.
     </p>
     <p>
-    <b>To express your interest</b>, kindly complete the <XLink href="https://docs.google.com/forms/d/e/1FAIpQLSdyJEc-yJUe3B45Bc_i0aPFcHMt_N98uu5x8fQtqmEJnNOtTQ/viewform?usp=header">participation form</XLink> by the <b>17 June 2026</b>.
+    <b>To express your interest</b>, kindly complete the <XLink href="https://docs.google.com/forms/d/e/1FAIpQLSdyJEc-yJUe3B45Bc_i0aPFcHMt_N98uu5x8fQtqmEJnNOtTQ/viewform?usp=header">participation form</XLink> by the <b>14 September 2026</b>.
     </p>
     <p>
     Coffee breaks and lunch will be provided by the organising team.
